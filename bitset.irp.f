fd17062461881d065988b3c4dd09655c01c409ef
v1jy4 <vijay.gopal.c@gmail.com>
2015-02-12 17:15:25 +0100

    stores add in arrays

--- bitset.irp.f
+++ bitset.irp.f
@@ -1,18 +1,16 @@
 program bitset
         implicit none
         integer(4)::i
-        integer::j,countbit,add,tmp,tmp2
-        
-        add=0
-        do i=1,10000
-        if(popcnt(i).eq.2)then
-        if(BTEST(i,0))then
-            write(6,15)i,i,i-(2**(popcnt(XOR(i,i-1))-1))
-            tmp=i-(2**(popcnt(XOR(i,i-1))-1))
-            tmp2=popcnt(XOR(tmp,tmp-1))-1
-            print *,2**(tmp2-1)+popcnt(XOR(i,i-1))-1
-            write(11,14)i,i
-        endif
+        integer::j,countbit,tmp,tmp2
+        integer,allocatable::add(:,:)
+        allocate(add(100,100))
+        countbit=0
+        do i=1,100
+        if(popcnt(i).eq.3)then
+            countbit+=1
+            add(countbit,1)=countbit
+            add(countbit,2)=i
+            write(6,16)add(countbit,2),add(countbit,1),add(countbit,2)
         endif
         enddo
 
@@ -24,4 +22,5 @@ program bitset
 12  FORMAT(I5,$)
 13  FORMAT(B64,B64)
 14  FORMAT(B64,I8)
+16  FORMAT(B64,I8,I8)
 end
